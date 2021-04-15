c17481efb2d78e38592bbc20f20ec34f7b14c78c
de4dcry <hodeadmusic@gmail.com>
2021-04-15 13:10:06 +0200

    pinta chulo

--- P2/index.js
+++ P2/index.js
@@ -12,8 +12,17 @@ const gui = {
     warning_display : document.getElementById("warning_display")
 };
 
+
+function estado(){
+    document.getElementById("estado_display").innerHTML = ESTADO;
+}
+
+t1 = setInterval(estado,100)
+
 let ESTADO = 0; /* 0= No hay nada escrito , 1= valor numerico , 2= operador , 3= segundo valor numerico
  (si se introducen mas operaciones se debe repetir el estado 2,3 )   */
+
+let EstadoAnterior = 0;
 
 let ANS = gui.display.innerHTML;
 
@@ -34,10 +43,17 @@ gui.botonEQUAL.onclick = () =>{
 
 gui.botonDEL.onclick = () => {
     gui.display.innerHTML = gui.display.innerHTML.slice(0,-1) ;
+    if (gui.display.innerHTML.length <= 0 ){
+        ESTADO = 0;
+        EstadoAnterior = 0;
+        gui.warning_display.innerHTML = "";
+    }
 }
 
 gui.botonAC.onclick = function () {
     gui.display.innerHTML = "0";
+    ESTADO = 0;
+    EstadoAnterior = 0;
 }
 
 function register_click (invalue){
@@ -54,9 +70,11 @@ for (let boton of gui.boton_numeric) {
     boton.onclick = (ev) => {
         if (ESTADO == 0){
             gui.display.innerHTML = "";
+            EstadoAnterior = ESTADO;
             ESTADO = 1;
             register_click(ev.target.value)}
         else if (ESTADO == 2){
+            EstadoAnterior = ESTADO;
             ESTADO = 3;
             register_click(ev.target.value);
         }
@@ -68,14 +86,21 @@ for (let boton of gui.boton_numeric) {
 
 for (let boton of gui.boton_operator) {
     boton.onclick = (ev) => {
-        register_click(ev.target.value)
+        
         if (ESTADO == 1 || ESTADO == 3){
+            EstadoAnterior = ESTADO;
+            register_click(ev.target.value);
             ESTADO = 2;
         }
         else if (ESTADO == 0){
+            gui.display.innerHTML = "";
+            ESTADO = 2;
+            register_click(ev.target.value)
             gui.warning_display.innerHTML = "No puede haber un operador antes de un valor numerico, corrigelo por favor";
         }
         else if (ESTADO == 2){
+            ESTADO = 2;
+            register_click(ev.target.value)
             gui.warning_display.innerHTML = "No puede haber dos operadores juntos, corrigelo por favor";
         }
 
