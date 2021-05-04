e82a8ec73a09dad73394573b106c5cfab378e374
de4dcry <hodeadmusic@gmail.com>
2021-05-04 19:25:32 +0200

    pa lante

--- P3/index.js
+++ P3/index.js
@@ -25,10 +25,12 @@ ctx.closePath();
 var bar = {
     x: 450,
     y: 700,
+    width: 100,
+    height:10,
     vx: 2,
     draw: function() {
         ctx.beginPath();
-        ctx.rect(this.x,this.y , 100, 10);
+        ctx.rect(this.x,this.y , this.with, this.height);
         ctx.fillStyle = 'green';
         ctx.fill();
         ctx.stroke();
@@ -36,33 +38,53 @@ var bar = {
     }
 };
 
+function colision(obj1 ,obj2){
+    for (i1 = obj1.x ; i1 != obj1.x + obj1.width ; i1++){
+        for (j1 = obj2.x ; j1= obj2.x + obj2.width ; j1++){
+            if (i1 == j1){
+                for(i2 = obj1.y ; i2 != obj1.y + obj1.width ; i2++){
+                    for (j2 = obj2.x ; j2= obj2.x + obj2.width ; j2++){
+                        if (i2 == j2){
+                            obj2.vx = - 1* obj2.vx
+                            obj2.vy = - 1* obj2.vy
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
 var ball = {
-    x: 400,
-    y: 500,
+    x_center: 400,
+    y_center: 500,
+    width: 10,
+    height:10,
+    x: 400 - this.width,
+    y: 500 -  this.width, 
     vx: 5,
     vy: -2,
     draw: function() {
 
-        ctx.clearRect(this.x -11 ,this.y -11, 22,22);
+        ctx.clearRect(this.x_center -11 ,this.y_center -11, 22,22);
 
-        if (this.x < 0 || this.x >= (canvas.width - 10) ) {
+        if (this.x_center < 0 || this.x_center >= (canvas.width - 10) ) {
             this.vx = -1  * this.vx;
           }
         
-        if (this.y <= 0) {
+        if (this.y_center <= 0) {
             this.vy = -1  * this.vy;
           }
 
-        this.x = this.x + this.vx;
-        this.y = this.y + this.vy;
+        this.x_center = this.x_center + this.vx;
+        this.y_center = this.y_center + this.vy;
         
         ctx.beginPath();
-        ctx.arc(this.x, this.y, 10, 0, 2 * Math.PI);
+        ctx.arc(this.x_center, this.y_center, 10, 0, 2 * Math.PI);
         ctx.fillStyle = 'yellow';
         ctx.fill();
         ctx.stroke();
-        ctx.closePath();       
-        
+        ctx.closePath();            
     }
 };
 
@@ -79,9 +101,6 @@ vida.onload = ()=> {
     ctx.drawImage(vida,850,750); 
 };
   
-var izquierda = document.getElementById("a");
-var derecha = document.getElementById("d");
-
 n = 0;
 window.onkeydown = (e) => {
     n = n+1;
@@ -95,11 +114,13 @@ window.onkeydown = (e) => {
         ctx.clearRect(bar.x -1 , bar.y -1 , 102,12);
             bar.x = bar.x + bar.vx + n;
         }
-   }
+}
   
 window.onkeyup = (e) => {
     n = 0;
   }
+
+
 
 function update() 
 {
