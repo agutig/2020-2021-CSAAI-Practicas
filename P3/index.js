de8ff7a109494d9720ef545bdd2cdf7ab5a12bad
de4dcry <hodeadmusic@gmail.com>
2021-05-06 10:11:16 +0200

    un retoquecito

--- P3/index.js
+++ P3/index.js
@@ -72,7 +72,7 @@ var eliminar = false;
 function colision(obj1 ,obj2){
     if(obj2.x >= obj1.x  &&  obj2.x <= obj1.x + obj1.width){
         ///Aqui, meterle caña
-        if(obj2.y + obj2.width >= obj1.y  &&  obj2.y + obj2.width <= obj1.y + obj1.height){
+        if(obj2.y + obj2.width >= obj1.y  &&  obj2.y - obj2.width <= obj1.y + obj1.height){
             eliminar = true;
             obj2.vx = -1* obj2.vx;
             obj2.vy = -1* obj2.vy;
