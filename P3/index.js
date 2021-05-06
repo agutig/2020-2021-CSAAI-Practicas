dbe463196ae0bad8c2d336434365c5e1f0317ddb
de4dcry <hodeadmusic@gmail.com>
2021-05-06 09:48:35 +0200

    ojoo

--- P3/index.js
+++ P3/index.js
@@ -67,11 +67,13 @@ for (let i = 0; i < LADRILLO.F; i++) {
     }
 }
 
+var eliminar = false;
+
 function colision(obj1 ,obj2){
     if(obj2.x >= obj1.x  &&  obj2.x <= obj1.x + obj1.width){
         ///Aqui, meterle caña
-        if(obj2.y + obj2.width >= obj1.y  &&  obj2.y + obj2.width <= obj1.y){
-            console.log("hey")
+        if(obj2.y + obj2.width >= obj1.y  &&  obj2.y + obj2.width <= obj1.y + obj1.height){
+            eliminar = true;
             obj2.vx = -1* obj2.vx;
             obj2.vy = -1* obj2.vy;
         }
@@ -83,9 +85,14 @@ function ladrillo_draw() {
     for (let i = 0; i < LADRILLO.F; i++) {
         for (let j = 0; j < LADRILLO.C; j++) {   
           if (ladrillos[i][j].visible == true) {
-
+            eliminar = false;
             colision(ladrillos[i][j] , ball);
+            if (eliminar == true){
+              ladrillos[i][j].visible = false;
+              eliminar = false;
+            }
 
+            ctx.clearRect(ladrillos[i][j].x, ladrillos[i][j].y , LADRILLO.width,LADRILLO.height);
             ctx.beginPath();
             ctx.rect(ladrillos[i][j].x, ladrillos[i][j].y, LADRILLO.width, LADRILLO.height);
             ctx.fillStyle = 'red';
