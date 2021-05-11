0c1c5958550b55869a9a52e649cd0333bacae436
de4dcry <hodeadmusic@gmail.com>
2021-05-11 23:23:16 +0200

    epale que empieza bien

--- P4/index.js
+++ P4/index.js
@@ -1,22 +1,13 @@
 //-- Obtener elementos del DOM
 const canvas = document.getElementById('canvas');
-const img = document.getElementById('imagesrc')
+const img = document.getElementById('imagesrc');
 const ctx = canvas.getContext('2d');
+let imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+let data = imgData.data
 
-//-- Función de retrollamada de imagen cargada
-//-- La imagen no se carga instantaneamente, sino que
-//-- lleva un tiempo. Sólo podemos acceder a ella una vez
-//-- que esté totalmente cargada
 img.onload = function () {
-
   console.log("Imagen cargada");
-
-  //-- Se establece como tamaño del canvas el mismo
-  //-- que el de la imagen original
   canvas.width = img.width;
   canvas.height = img.height;
-
-  //-- Situar la imagen original en el canvas
-  //-- No se han hecho manipulaciones todavia
-  ctx.drawImage(img, 0,0);
+  ctx.drawImage(img, 0,0 ,img.width,img.height);
 };
