05d5f00b3273b6366fa0157d6a8ef2f482a75a2b
de4dcry <hodeadmusic@gmail.com>
2021-05-12 00:31:42 +0200

    ya empiezan los errores

--- P4/index.js
+++ P4/index.js
@@ -1,9 +1,12 @@
 //-- Obtener elementos del DOM
 const canvas = document.getElementById('canvas');
 const img = document.getElementById('imagesrc');
+const boton_gris = document.getElementById('gris');
+let Estado_gris = 0;
 const ctx = canvas.getContext('2d');
 let imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-let data = imgData.data
+let data = imgData.data;
+const original_colors = data;
 
 img.onload = function () {
   console.log("Imagen cargada");
@@ -11,3 +14,28 @@ img.onload = function () {
   canvas.height = img.height;
   ctx.drawImage(img, 0,0 ,img.width,img.height);
 };
+
+function gray_er(){
+  for (let i = 0; i < data.length; i+=4) {
+      data[i] =  data[i] +10; 
+      data[i+1] = data[i+1] + 10;
+      data[i+2] = data[i+2] +10;
+  }
+
+}
+
+boton_gris.onclick = () => {
+    if (Estado_gris == 0){
+      boton_gris.style.backgroundColor= "orange";
+      gray_er;
+      ctx.putImageData(imgData, 0, 0);
+      Estado_gris = 1;
+    }
+    else{
+      boton_gris.style.backgroundColor= "gray";
+      data = original_colors;
+      ctx.putImageData(imgData, 0, 0);
+      Estado_gris = 0;
+    }
+
+}
