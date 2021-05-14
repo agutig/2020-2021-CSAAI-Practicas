fb9d3240ac36d4d719d9d783a5c849e77da61e30
de4dcry <hodeadmusic@gmail.com>
2021-05-14 15:54:56 +0200

    varios pasos atras, ya veremos para alante

--- P4/index.js
+++ P4/index.js
@@ -1,25 +1,28 @@
 //-- Obtener elementos del DOM
+
+
 const canvas = document.getElementById('canvas');
 const img = document.getElementById('imagesrc');
 const boton_gris = document.getElementById('gris');
-let Estado_gris = 0;
 const ctx = canvas.getContext('2d');
-let imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-let data = imgData.data;
-console.log(canvas.width);
-console.log(canvas.height);
-const original_colors = data.slice();
+
 
 img.onload = function () {
-  console.log("Imagen cargada");
-  canvas.width = img.width;
-  canvas.height = img.height;
-  ctx.drawImage(img, 0,0 ,img.width,img.height);
+  ctx.drawImage(img, 0,0 );
 };
+
+canvas.width = img.width;
+canvas.height = img.height;
+let imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+let data = imgData.data;
+let Estado_gris = 0;
+const original_colors = data.slice();
+
+
 
 function gray_er(){
   for (let i = 0; i < data.length; i+=4) {
-     
       data[i] =  data[i] +10; 
       data[i+1] = data[i+1] + 10;
       data[i+2] = data[i+2] +10;
