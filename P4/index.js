6aa669eebe7db30ea915e9bfafad0aa8341cd5ab
de4dcry <hodeadmusic@gmail.com>
2021-05-14 18:15:55 +0200

    profe,  que musica le gusta?

--- P4/index.js
+++ P4/index.js
@@ -5,6 +5,7 @@ const canvas = document.getElementById('canvas');
 const img = document.getElementById('imagesrc');
 const ctx = canvas.getContext('2d');
 const boton_gris = document.getElementById('gris');
+const boton_colores = document.getElementById('colores');
 
 img.onload = function () {
     canvas.width = img.width;
@@ -17,31 +18,59 @@ let data = imgData.data;
 console.log(data);
 const original_colors = data.slice();
 let Estado_gris = 0;
-
+let Estado_colores = 0;
 
 function gray_maker (){
-
+  for (let i = 0; i < data.length; i+=4) {
+    brillo = (3 * data[i] + 4 * data[i+1] + data[i+2])/8
+    data[i] =  brillo ; 
+    data[i+1] = brillo ;
+    data[i+2] = brillo ;
+  }
 }
 
 boton_gris.onclick = () => {
   ctx.drawImage(img, 0,0 );
   imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
   data = imgData.data;
-
+  Estado_colores= 0;
+  boton_colores.style.backgroundColor= "white";
     if (Estado_gris == 0){
       boton_gris.style.backgroundColor= "orange";
 
-//CODIGO PARA LA MODIFICACION DE LA IMAGEN
+      //CODIGO PARA LA MODIFICACION DE LA IMAGEN
       gray_maker();
-//////////////////////////////////////////
+
       ctx.putImageData(imgData,0, 0);
       Estado_gris = 1;
     }
     else{
-      boton_gris.style.backgroundColor= "blue";
+      boton_gris.style.backgroundColor= "white";
       data = original_colors.slice();
       ctx.putImageData(imgData, 0, 0);
       Estado_gris = 0;
+    }
+
+}
+
+
+boton_colores.onclick = () => {
+  ctx.drawImage(img, 0,0 );
+  imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+  data = imgData.data;
+  Estado_gris = 0;
+  boton_gris.style.backgroundColor= "white";
+    if (Estado_colores == 0){
+      boton_colores.style.backgroundColor= "orange";
+
+      ctx.putImageData(imgData,0, 0);
+      Estado_colores = 1;
+    }
+    else{
+      boton_colores.style.backgroundColor= "white";
+      data = original_colors.slice();
+      ctx.putImageData(imgData, 0, 0);
+      Estado_colores = 0;
     }
 
 }
